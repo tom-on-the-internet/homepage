bb0266e262d26da9141f621c62a8019d587885df
Thomas Steven <thomaspatricksteven@gmail.com>
2021-12-29 21:00:15 -0500

    initialize repo

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log(
+  "%ccurl https://business-card.tomontheinternet.com",
+
+  "color:green;font-family:monospace; font-size: 20px"
+);
+
+// tomlink can be used to get the info you need
+// make an endpoint
+// country, city, that's it.
