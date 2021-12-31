7b1aa3186b1ebeeb6e836ddba55837513e6ea01f
Thomas Steven <thomaspatricksteven@gmail.com>
2021-12-30 22:12:30 -0500

    about you

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 async function getUserInformation() {
+    let userInfo = {}
+
     try {
         let res = await fetch('https://tomlink.ca/identify')
-        res = await res.json()
-        console.log(res)
+        userInfo = await res.json()
     } catch (err) {
         console.log(err)
     }
+
+    var parsedResults = new UAParser().getResult()
+    console.log(parsedResults)
+
+    userInfo.browser = parsedResults.browser.name
+    userInfo.os = osDescription(parsedResults.os.name)
+
+    decorateAboutYou(userInfo)
+    unhideAboutYou()
+
+    console.log(userInfo)
 }
 
 function businessCard() {
@@ -14,6 +26,45 @@ function businessCard() {
 
         'color:green;font-family:monospace; font-size: 20px'
     )
+}
+
+function decorateAboutYou(userInfo) {
+    document.querySelector('#about-you-os').textContent = userInfo.os
+    document.querySelector('#about-you-browser').textContent = userInfo.browser
+    document.querySelector('#about-you-country').textContent = userInfo.country
+    document.querySelector('#about-you-city').textContent = userInfo.city
+}
+
+function unhideAboutYou() {
+    document.querySelector('#about-you').classList.remove('hide')
+}
+
+function osDescription(os) {
+    if (os == 'iOS') {
+        return `You're holding your pretty iPhone in your hands. Perhaps feeling less convinced that Apple makes secure products.`
+    }
+
+    if (os.includes('Android')) {
+        return `You're holding your Android in your hands. Maybe if you'd gotten an iPhone you'd be more secure.`
+    }
+
+    if (os.includes('Windows')) {
+        return `You're sitting in front of your Windows PC. It's just you, me, and Bill Gates here now.`
+    }
+
+    if (os === 'Arch') {
+        return `You're sitting in front of your computer. BTW, did you know you use Arch?`
+    }
+
+    if (os === 'Mac OS') {
+        return `You're sitting in front of your Mac. Perhaps feeling less convinced that Apple makes secure products.`
+    }
+
+    if (os === 'Linux') {
+        return `You're sitting in front of your Linux box. Perhaps feeling like a little bit less of a hacker all of a sudden.`
+    }
+
+    return 'Your computer is unconventional.'
 }
 
 getUserInformation()
